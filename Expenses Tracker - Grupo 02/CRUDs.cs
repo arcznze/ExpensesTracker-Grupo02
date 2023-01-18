e599fd10dc00cf11208336d4b4cbfe76ca1b5c9d
arcznze <87095931+arcznze@users.noreply.github.com>
2023-01-18 16:25:49 -0400

    CRUD de las categorias listo.

--- Expenses Tracker - Grupo 02/CRUDs.cs
+++ Expenses Tracker - Grupo 02/CRUDs.cs
@@ -7,14 +7,14 @@ using System.Threading.Tasks;
 
 namespace Expenses_Tracker___Grupo_02
 {
-    public class Accounts
+    public class CRUDs
     {
-        public string createAccount(List<string> x, string y)
+        public string create(List<string> x, string y)
         {
             x.Add(y);
             return y;
         }
-        public string editAccount(List<string> x, string y, string z)
+        public string edit(List<string> x, string y, string z)
         {
             int index = x.IndexOf(y);
             x.RemoveAt(index);
@@ -22,11 +22,11 @@ namespace Expenses_Tracker___Grupo_02
 
             return x.ToString();
         }
-        public void deleteAccount(List<string> x, string y)
+        public void delete(List<string> x, string y)
         {
             x.Remove(y);
         }
-        public void readAccount(List<string> x)
+        public void read(List<string> x)
         {
             foreach (string word in x)
             {
--- Expenses Tracker - Grupo 02/Program.cs
+++ Expenses Tracker - Grupo 02/Program.cs
@@ -2,14 +2,15 @@
 using System.Diagnostics.Tracing;
 using System;
 using System.Collections.Generic;
-using static Expenses_Tracker___Grupo_02.Accounts;
+using static Expenses_Tracker___Grupo_02.CRUDs;
 using Spectre.Console;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Security.Principal;
 using Expenses_Tracker___Grupo_02;
 
-List<string> listAux = new List<string>();
-Accounts aux = new Accounts();
+List<string> listAccount = new List<string>();
+List<string> listCategory = new List<string>();
+CRUDs aux = new CRUDs();
 
 //MENU PRINCIPAL
 var tableTitle = new Table();
@@ -48,23 +49,24 @@ switch (option)
         switch (option)
         {
             case "New transaction.":
-                Console.WriteLine("Enter the name of the transaction: ");
+                Console.Write("Enter the name of the transaction: ");
                 var nameTransaction = Console.ReadLine();
                 Console.Write("\nType [Expense/Income]: ");
                 var type = Console.ReadLine();
                 var account = "";
-                if (listAux.Count == 0)
+                var category = "";
+                if (listAccount.Count == 0)
                 {
                     Console.WriteLine("\nIt looks like you haven't created an account type yet");
                     Console.Write("What kind of account is it? ");
                     account = Console.ReadLine();
-                    aux.createAccount(listAux, account);
+                    aux.create(listAccount, account);
 
                 }
                 else
                 {
                     var accountAux = new SelectionPrompt<string>();
-                    foreach (var accounts in listAux)
+                    foreach (var accounts in listAccount)
                     {
                         accountAux.AddChoice(accounts);
                     }
@@ -76,13 +78,40 @@ switch (option)
                     {
                         Console.Write("\nWhat kind of account is it?");
                         account = Console.ReadLine();
-                        aux.createAccount(listAux, account);
+                        aux.create(listAccount, account);
                     }
                 }
 
                 Console.Write("\nType of account [Savings/Current/etc...]: " + account.ToString() + "\n");
-                Console.Write("\nCategory: ");
-                var category = Console.ReadLine();
+
+                if (listCategory.Count == 0)
+                {
+                    Console.WriteLine("\nIt looks like you haven't created a category type yet");
+                    Console.Write("What kind of category is it? ");
+                    category = Console.ReadLine();
+                    aux.create(listCategory, category);
+
+                }
+                else
+                {
+                    var categoryAux = new SelectionPrompt<string>();
+                    foreach (var categories in listCategory)
+                    {
+                        categoryAux.AddChoice(categories);
+                    }
+                    categoryAux.AddChoice("Add new category");
+
+                    category = AnsiConsole.Prompt(categoryAux);
+
+                    if (category == "Add new category")
+                    {
+                        Console.Write("\nWhat kind of category is it?");
+                        category = Console.ReadLine();
+                        aux.create(listCategory, category);
+                    }
+                }
+
+                Console.Write("\nCategory: " + category + "\n");
                 Console.Write("\nAmount: ");
                 var amount = Console.ReadLine();
                 Console.Write("\nDescription: ");
@@ -117,7 +146,7 @@ switch (option)
             case "New account.":
                 Console.Write("What kind of account is it? ");
                 var newAccount = Console.ReadLine();
-                aux.createAccount(listAux, newAccount);
+                aux.create(listAccount, newAccount);
 
                 Console.WriteLine("Account created.\n");
                 option = AnsiConsole.Prompt(
@@ -133,6 +162,22 @@ switch (option)
                 }
                 break;
             case "New category.":
+                Console.Write("What kind of category is it? ");
+                var newCategory = Console.ReadLine();
+                aux.create(listCategory, newCategory);
+
+                Console.WriteLine("Category created.\n");
+                option = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                    .AddChoices(new[] {
+                            "Back", "Exit"
+                    }));
+                switch (option)
+                {
+                    case "Back":
+                        Console.Clear();
+                        goto NewItem;
+                }
                 break;
             case "Back":
                 Console.Clear();
@@ -154,7 +199,7 @@ switch (option)
             case "View transactions.":
                 break;
             case "View accounts.":
-                aux.readAccount(listAux);
+                aux.read(listAccount);
                 Console.WriteLine("\n");
                 option = AnsiConsole.Prompt(
                 new SelectionPrompt<string>()
@@ -169,14 +214,25 @@ switch (option)
                 }
                 break;
             case "View categories.":
+                aux.read(listCategory);
+                Console.WriteLine("\n");
+                option = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                    .AddChoices(new[] {
+                        "Back", "Exit"
+                    }));
+                switch (option)
+                {
+                    case "Back":
+                        Console.Clear();
+                        goto ViewItems;
+                }
                 break;
             case "Back":
                 goto Menu;
             default:
                 break;
         }
-
-
         break;
     case "Edit items.":
     EditItems:
@@ -191,12 +247,15 @@ switch (option)
             case "Edit transactions.":
                 break;
             case "Edit accounts.":
-
+                foreach (string accounts in listAccount)
+                {
+                    Console.Write(accounts);
+                }
                 Console.Write("Which account would you like to change? ");
                 var oldAccount = Console.ReadLine();
                 Console.Write("To which? ");
                 var newAccount = Console.ReadLine();
-                aux.editAccount(listAux, oldAccount, newAccount);
+                aux.edit(listAccount, oldAccount, newAccount);
 
                 Console.WriteLine("Changes made.");
                 option = AnsiConsole.Prompt(
@@ -212,6 +271,28 @@ switch (option)
                 }
                 break;
             case "Edit categories.":
+                foreach (string category in listCategory)
+                {
+                    Console.Write(category);
+                }
+                Console.Write("\nWhich category would you like to change? ");
+                var oldCategory = Console.ReadLine();
+                Console.Write("To which? ");
+                var newCategory = Console.ReadLine();
+                aux.edit(listCategory, oldCategory, newCategory);
+
+                Console.WriteLine("Changes made.");
+                option = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                    .AddChoices(new[] {
+                            "Back", "Exit"
+                    }));
+                switch (option)
+                {
+                    case "Back":
+                        Console.Clear();
+                        goto EditItems;
+                }
                 break;
             case "Back":
                 goto Menu;
@@ -234,17 +315,18 @@ switch (option)
             case "Delete transactions.":
                 break;
             case "Delete accounts.":
+                Console.WriteLine("Select the accounts you want to remove.");
                 var deletedAccount = new MultiSelectionPrompt<string>().NotRequired();
-                foreach (var accounts in listAux)
+                foreach (var accounts in listAccount)
                 {
                     deletedAccount.AddChoice(accounts);
                 }
 
                 var deletedAccounts = AnsiConsole.Prompt(deletedAccount);
 
-                foreach (string fruit in deletedAccounts)
+                foreach (string accounts in deletedAccounts)
                 {
-                    aux.deleteAccount(listAux, fruit);
+                    aux.delete(listAccount, accounts);
                 }
 
                 Console.WriteLine($"You delete those accounts.");
@@ -263,6 +345,34 @@ switch (option)
                 }
                 break;
             case "Delete categories.":
+                Console.WriteLine("Select the categories you want to remove.");
+                var deletedCategory = new MultiSelectionPrompt<string>().NotRequired();
+                foreach (var categories in listCategory)
+                {
+                    deletedCategory.AddChoice(categories);
+                }
+
+                var deletedCategories = AnsiConsole.Prompt(deletedCategory);
+
+                foreach (string categories in deletedCategories)
+                {
+                    aux.delete(listCategory, categories);
+                }
+
+                Console.WriteLine($"You delete those categories.");
+
+                option = AnsiConsole.Prompt(
+            new SelectionPrompt<string>()
+                .AddChoices(new[] {
+                    "Back", "Exit"
+                }));
+
+                switch (option)
+                {
+                    case "Back":
+                        Console.Clear();
+                        goto DeletedItems;
+                }
                 break;
             case "Back":
                 goto Menu;
