544d73ed149e5fcccc8920d14d51a6b3e9d6e181
Camilo María <camilo_jose08@hotmail.com>
2023-01-22 19:25:18 -0400

    Agregue prueba unitaria de newAccount, newCategory y editTransaction

--- TestExpensesTracker/UnitTest1.cs
+++ TestExpensesTracker/UnitTest1.cs
@@ -36,38 +36,50 @@ namespace TestExpensesTracker
         }
 
         [TestMethod]
-        public void TestViewTransactions()
+        public void TestEditTransaction()
         {
-            //Arrange
-            //Add some test transactions to _listTransaction
+            // Arrange
+            CRUDs aux = new CRUDs();
+            // Crear una transacción de prueba
             _listTransaction.Add(new Transaction
             {
-                Name = "TestTransaction1",
+                Name = "Test Transaction",
                 Type = "Expense",
                 Account = "Savings",
-                Category = "Home",
+                Category = "Bills",
                 Amount = 100,
                 Description = "Test Description",
                 Date = DateTime.Now
             });
-            _listTransaction.Add(new Transaction
+
+            // Act
+            var editedTransactions = new List<string> { "Test Transaction" };
+            foreach (var transactions in editedTransactions)
             {
-                Name = "TestTransaction2",
-                Type = "Income",
-                Account = "Current",
-                Category = "Bills",
-                Amount = 200,
-                Description = "Test Description",
-                Date = DateTime.Now
-            });
-            var expectedResult = "Transaction Viewed successfully.";
+                var transactionToEdit = _listTransaction.FirstOrDefault(t => t.Name == transactions);
+                var editOption = "Amount"; // Seleccionar opcion de edicion
+                var editOption2 = "Category"; // Seleccionar opcion de edicion
+                switch (editOption)
+                {
+                    case "Amount":
+                        var newAmount = 150;
+                        transactionToEdit.Amount = newAmount;
+                        break;
+                }
+                switch (editOption2)
+                {
+                    case "Category":
+                        var newCategory = "Homes";
+                        transactionToEdit.Category = newCategory;
+                        break;
+                }
+            }
+            // Obtener la transacción editada
+            var editedTransaction = _listTransaction.FirstOrDefault(t => t.Name == "Test Transaction");
 
-            //Act
-            Program.ViewItems();
-            var result = Console.Out.ToString();
-
-            //Assert
-            StringAssert.Contains(result, expectedResult);
+            // Assert
+            Assert.AreEqual(150, editedTransaction.Amount);
+            Assert.AreEqual("Homes", editedTransaction.Category);
         }
     }
 }
